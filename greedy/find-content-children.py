40a386fba9adc57079d8473fbc99d12e5dbed7d1
fooldot <northill1999@gmail.com>
2026-08-28 10:11:33 +0800

    greedy-b

---
+++ greedy/find-content-children.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        s.sort()
+        g.sort()
+
+        child = 0
+        cookie = 0
+
+        while child < len(g) and cookie < len(s):
+            if g[child] <= s[cookie]:
+                child += 1
+            cookie += 1
+        return child
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    
---
+++ greedy/longest-palindrome.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        count = {}
+        for i in s:
+            count[i] = count.get(i, 0) + 1
+
+        length = 0
+        odd_flag = 0
+        final = 0
+        for c in count.values():
+            length = c // 2 * 2
+            if c % 2 == 1:
+                odd_flag = 1
+            final += length
+
+        return final + odd_flag
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.longestPalindrome("abba"))
