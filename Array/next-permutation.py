e00a65a83be1ad446670285155e8293719d2efcd
fooldot <northill1999@gmail.com>
2026-09-03 13:34:23 +0800

    permutation-1

--- Array/next-permutation.py
+++ Array/next-permutation.py
@@ -4,7 +4,26 @@ from typing import List
 
 class Solution:
     def nextPermutation(self, nums: List[int]) -> List:
-        # to be continue
+        n = len(nums)
+
+        i = n - 2
+        while i >= 0 and nums[i] >= nums[i+1]:
+            i -= 1
+
+        if i >= 0:
+            j = n - 1
+            # 破坏升序 -> 右侧必然存在比之更大的数字
+            while j >= 0 and nums[j] <= nums[i]:  # find the first bigger one than nums[i] 
+                j -= 1
+
+            nums[i], nums[j] = nums[j], nums[i]
+
+        left = i + 1
+        right = n - 1
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
 
         return nums
 
