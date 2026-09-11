0cb6dbcc90b77f15b7a887ac6b9d7177f5ac0374
fooldot <northill1999@gmail.com>
2026-09-11 16:06:47 +0800

    climb-stairs

---
+++ DP/climb-stairs-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        # if n <= 2:
+        #     return n
+
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+
+# f(n) = f(n-1) + f(n-2)
--- DP/climb-stairs.py
+++ DP/climb-stairs.py
@@ -1,7 +1,24 @@
+# https://leetcode.cn/problems/climbing-stairs/
+
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
-        return
+        cnt = 0
+        def fcn1(n1):
+            nonlocal cnt
+
+            if n1 > n:
+                return
+            if n1 == n:
+                cnt += 1
+                return
+
+            fcn1(n1 + 1)
+
+            fcn1(n1 + 2)
+
+        fcn1(0)
+
+        return cnt
 
 
 if __name__ == "__main__":
