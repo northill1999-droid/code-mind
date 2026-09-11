fba4cbc3f8c24eeebefb05e30ee6f2e55799157f
fooldot <northill1999@gmail.com>
2026-09-11 16:08:05 +0800

    climb-stairs

--- DP/climb-stairs-pd.py
+++ DP/climb-stairs-pd.py
@@ -6,7 +6,7 @@ class Solution:
         dp = [0] * (n + 1)
 
         dp[1], dp[2] = 1, 2
-        for i in range(3, n + 1):
+        for i in range(3, n + 1):  # f(n) = f(n-1) + f(n-2)
             dp[i] = dp[i-2] + dp[i-1]
         
         return dp[n]
@@ -16,3 +16,4 @@ if __name__ == "__main__":
     sol = Solution()
     print(sol.climbStairs(2))
     print(sol.climbStairs(3))
+    
