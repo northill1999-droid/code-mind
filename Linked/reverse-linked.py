8712e84d4b70a2b03f345aacf106964a6ded722f
fooldot <northill1999@gmail.com>
2026-09-12 09:52:24 +0800

    linked-reverse

--- Linked/reverse-linked.py
+++ Linked/reverse-linked.py
@@ -1,3 +1,5 @@
+# https://leetcode.cn/problems/UHnkqh/
+
 from typing import Optional
 
 class ListNode:
@@ -5,4 +7,43 @@ class ListNode:
         self.val = val
         self.next = next
 
-        
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+
+        pre = None
+        cur = head
+        while cur:
+            nxt = cur.next
+
+            cur.next = pre
+            pre = cur
+
+            cur = nxt
+
+        return pre
+
+def lst_to_linked(lst):
+    if not lst:
+        return None
+
+    head = ListNode(lst[0])
+    cur = head
+
+    for i in lst[1:]:
+        cur.next = ListNode(i)
+        cur = cur.next
+
+    return head
+
+
+def linked_to_lst(head):
+    res = []
+    while head:
+        res.append(head.val)
+        head = head.next
+
+    return res
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(linked_to_lst(sol.reverseList(lst_to_linked([1,2,3,4,5]))))
