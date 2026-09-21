a1976a7f05b50beec4e8caae769fada393bb927f
fooldot <northill1999@gmail.com>
2026-09-21 08:36:58 +0800

    binary-traversal

---
+++ binary-search/inorder-traversal.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode | None) -> List[int]:
+        result = []
+
+        def dfs(node):
+            if not node:
+                return
+
+            dfs(node.left)
+            result.append(node.val)
+            dfs(node.right)
+
+        dfs(root)
+        return result
+    
