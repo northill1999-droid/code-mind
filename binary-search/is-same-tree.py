5429208e36f59c984f370de5f964861421ee7d5e
fooldot <northill1999@gmail.com>
2026-09-23 01:58:28 +0800

    binary-same-tree

---
+++ binary-search/is-same-tree.py
@@ -0,0 +1,22 @@
+# https://leetcode.cn/problems/same-tree/solutions/363636/xiang-tong-de-shu-by-leetcode-solution/
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSameTree(self, p: TreeNode | None, q: TreeNode | None) -> bool:
+        if not p and not q:
+            return True
+
+        elif not p or not q:
+            return False
+
+        elif p.val != q.val:
+            return False
+
+        else:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
