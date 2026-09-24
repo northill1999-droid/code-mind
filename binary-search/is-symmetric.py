de24949fb87d713aacdd2aa2ea3e0e9038c1cd3a
fooldot <northill1999@gmail.com>
2026-09-24 10:04:54 +0800

    binary-symmetric

---
+++ binary-search/is-symmetric.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def check(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+
+        if not p or not q:
+            return False
+
+        return p.val == q.val and self.check(p.left, q.right) and self.check(p.right, q.left)
+
+    def isSymmetric(self, root: TreeNode) -> bool:
+        return self.check(root.left, root.right)
+    
