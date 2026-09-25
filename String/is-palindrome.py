7f1cfde8444947cbf477aac8f8d57880d1d18515
fooldot <northill1999@gmail.com>
2026-09-25 09:31:17 +0800

    palindrome

---
+++ String/is-palindrome.py
@@ -0,0 +1,8 @@
+# https://leetcode.cn/problems/valid-palindrome/
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        filtered_chars = [char.lower() for char in s if char.isalnum()]
+
+        return filtered_chars == filtered_chars[::-1]
+    
